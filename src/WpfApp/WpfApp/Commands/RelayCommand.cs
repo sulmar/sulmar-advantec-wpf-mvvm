b7ecaa060c27c8788e6658a70b8b11f8a768e386
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-06-25 13:15:42 +0200

    Dodanie RaiseCanExecuteChanged

--- src/WpfApp/WpfApp/Commands/RelayCommand.cs
+++ src/WpfApp/WpfApp/Commands/RelayCommand.cs
@@ -24,4 +24,9 @@ public class RelayCommand : ICommand
     {
         execute?.Invoke(parameter);
     }
+
+    public void RaiseCanExecuteChanged()
+    {
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
 }
