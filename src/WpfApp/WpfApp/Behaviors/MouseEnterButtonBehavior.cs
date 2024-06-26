273a245de52167963030a5c128f28190e3344986
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-06-26 15:01:06 +0200

    Dodanie Behavior

---
+++ src/WpfApp/WpfApp/Behaviors/MouseEnterButtonBehavior.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WpfApp.Behaviors;
+
+// Install-Package Microsoft.Xaml.Behaviors.Wpf
+class MouseEnterButtonBehavior : Behavior<Button>
+{
+    public System.Windows.Media.Color HighlightColor { get; set; }
+
+    private System.Windows.Media.Brush _brush;
+    protected override void OnAttached()
+    {
+        Button button = this.AssociatedObject;
+
+        button.MouseEnter += (sender, e) => {
+            _brush = button.Background;
+            button.Background = new SolidColorBrush(HighlightColor); 
+        };
+
+       // button.MouseLeave += (sender, e) => { button.Background = _brush; };
+
+    }
+}
